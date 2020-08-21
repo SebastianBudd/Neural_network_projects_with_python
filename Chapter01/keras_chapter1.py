d939864e31cc5f61805ec446eaac73a553ada99a
Seb Budd <sebastianbudd@btinternet.com>
2020-08-21 12:06:44 +0100

    Pycharm additions

--- Chapter01/keras_chapter1.py
+++ Chapter01/keras_chapter1.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras import optimizers
 import numpy as np
+
 np.random.seed(9)
 
 model = Sequential()
@@ -17,11 +18,11 @@ print('')
 sgd = optimizers.SGD(lr=1)
 model.compile(loss='mean_squared_error', optimizer=sgd)
 
-X = np.array([[0,0,1],
-              [0,1,1],
-              [1,0,1],
-              [1,1,1]])
-y = np.array([[0],[1],[1],[0]])
+X = np.array([[0, 0, 1],
+              [0, 1, 1],
+              [1, 0, 1],
+              [1, 1, 1]])
+y = np.array([[0], [1], [1], [0]])
 
 model.fit(X, y, epochs=1500, verbose=False)
 
